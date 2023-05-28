cf4f30e308ca0e3f8bc2d2a1a3ea75638b0a96f0
Nikhil Patel <nikhilid2205@gmail.com>
2023-05-28 16:23:51 +0530

    spelling mistake of function name

--- src/ApiError.js
+++ src/ApiError.js
@@ -1,6 +1,7 @@
-export default function ApiEror() {
+import "./basic.css";
+export default function ApiError() {
   return (
-    <div>
+    <div className="form_content">
       <h1> Error in API-KEY or Network Error or Request Issue.</h1>
     </div>
   );
